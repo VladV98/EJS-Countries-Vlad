6c3374bbd1ff9459dfdfb31b5e3a30388eb6e32b
V1ad98 <vladvoloshko@list.ru>
2020-03-15 14:31:24 +0200

    some fixes

--- server.js
+++ server.js
@@ -1,29 +1,79 @@
-const express = require("express");
-const axios = require("axios");
-const bodyParser = require('body-parser')
+const express = require('express');
+// const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require('body-parser');
+const path = require('path');
+const request = require('request');
+const router = express.Router();
 const app = express();
-
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname+"/index.html")
+
+
+app.get('/', function(req, res){
+  res.render('index');
 });
 
-app.post("/", function(req, res){
-    let country = "Estonia";
-    let ddl = req.body.ddl;
-    let url = `https://restcountries.eu/rest/v2/name/Estonia?fullText=true`;
-
-    axios.get(url).then(function(response){
-        let Name;
-        console.log("Server status code " , response.status);
-        if(ddl === "ee"){
-            Name = response.name;
-            console.log(Name);
-        }
-    });
+app.post('', function(req, res){
+    let choice = req.body.ddl;
+    url = `https://restcountries.eu/rest/v2/name/${choice}?fullText=true`;
+  request(url, function(error, response, body){
+    let data = JSON.parse(response.body);
+    if(choice === 'ee'){
+      res.render('result', {
+        country : data[0].name,
+        level : data[0].topLevelDomain[0],
+        callingCode : data[0].callingCodes[0],
+        capital : data[0].capital,
+        region : data[0].region,
+        subregion : data[0].subregion,
+        population : data[0].population,
+        timezone : data[0].timezones[0],
+        language : data[0].altSpellings[2],
+        currency1 : data[0].currencies[0].code,
+        currency2 : data[0].currencies[0].name,
+        currency3 : data[0].currencies[0].symbol,
+        flag : data[0].flag
+      });
+    }
+    else if(choice === 'lv'){
+      res.render('result', {
+        country : data[0].name,
+        level : data[0].topLevelDomain[0],
+        callingCode : data[0].callingCodes[0],
+        capital : data[0].capital,
+        region : data[0].region,
+        subregion : data[0].subregion,
+        population : data[0].population,
+        timezone : data[0].timezones[0],
+        language : data[0].altSpellings[2],
+        currency1 : data[0].currencies[0].code,
+        currency2 : data[0].currencies[0].name,
+        currency3 : data[0].currencies[0].symbol,
+        flag : data[0].flag
+      });
+    }
+    else if(choice === 'lt'){
+      res.render('result', {
+        country : data[0].name,
+        level : data[0].topLevelDomain[0],
+        callingCode : data[0].callingCodes[0],
+        capital : data[0].capital,
+        region : data[0].region,
+        subregion : data[0].subregion,
+        population : data[0].population,
+        timezone : data[0].timezones[0],
+        language : data[0].altSpellings[2],
+        currency1 : data[0].currencies[0].code,
+        currency2 : data[0].currencies[0].name,
+        currency3 : data[0].currencies[0].symbol,
+        flag : data[0].flag
+      });
+    }
+  });
 });
-
 app.listen(3000, function(){
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
